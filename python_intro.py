358cc8cbe42989c7a5fe71df658952a314770950
csisemore <camilla.sisemore@gmail.com>
2024-10-12 13:21:54 -0500

    First Commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+def hi(name):
+    print("Hi " + name + "!")
+
+
+names = ["Rachel", "Tom", "Phoebe", "Stephen", "You"]
+for name in names:
+    hi(name)
+    print("Next name")
+
+for i in range(1, 6):
+    print(i)
